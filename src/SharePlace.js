3101c959171ecdb8fc190805fec08cdb20592602
rocionadal <rocioonadal@gmail.com>
2021-03-28 19:25:46 +0200

    modal file and logic to display it

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -1,3 +1,5 @@
+import { Modal } from './UI/Modal';
+
 class PlaceFinder {
   constructor() {
     const addressForm = document.querySelector('form');
@@ -12,14 +14,18 @@ class PlaceFinder {
       alert('Location feature is not available in your browser - please use a more modern browser or manually enter an address');
       return;
     }
+    const modal = new Modal('loading-modal-content', 'Loading location - please wait!');
+    modal.show();
     navigator.geolocation.getCurrentPosition(
       successResult => {
+        modal.hide();
         const coordinates = {
           lat: successResult.coords.latitude,
           lng: successResult.coords.longitude
         };
         console.log(coordinates);
       }, error => {
+        modal.hide();
         alert('Could not locate you unfortunately. Please enter an address manually!');
       }
     );
